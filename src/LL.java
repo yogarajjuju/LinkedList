e64f25261af36a5b2b83026df2b452165d491233
yogarajjuj <yogarajjuju22@gmail.com>
2025-08-29 10:34:09 +0530

    bankai

--- src/LL.java
+++ src/LL.java
@@ -7,6 +7,26 @@ public class LL {
     public LL() {
         this.size=0;
     }
+    // to insert a new node in head
+    public void insertFirst(int value){
+        Node node = new Node(value);
+        node.next=head;
+        head=node;
+        if(tail==null){
+            tail=head;
+
+        }
+        size+=1;
+    }
+
+    public void display(){
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.value+ " -> ");
+            temp=temp.next;
+        }
+        System.out.println("END");
+    }
 
     private class Node{
         private int value;
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        LL list = new LL();
+        list.insertFirst(3);
+        list.insertFirst(5);
+        list.insertFirst(8);
+        list.insertFirst(11);
+
+        list.display();
+
 
     }
 }
