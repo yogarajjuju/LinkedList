1b135ce916ad45462f7535b0f7dd7e07d1d9f564
yogarajjuj <yogarajjuju22@gmail.com>
2025-09-04 10:42:41 +0530

    bankani

--- src/LL.java
+++ src/LL.java
@@ -53,22 +53,21 @@ public class LL {
     }
 
     //Insertion using recursion
-    public void insertrec(int val,int index){
-
+    // insert using recursion
+    public void insertRec(int val, int index) {
+        head = insertRec(val, index, head);
     }
-    private Node insertrec(int val , int index, Node node){
-        if(index==0){
-            Node temp = new Node(val,node);
+    private Node insertRec(int val, int index, Node node) {
+        if (index == 0) {
+            Node temp = new Node(node, val);
             size++;
             return temp;
-
         }
 
-
-       node.next= insertrec(val,index--,node.next);
+        node.next = insertRec(val, index-1, node.next);
         return node;
-
     }
+
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-//        LL list = new LL();
-//        list.insertFirst(3);
-//        list.insertFirst(5);
-//        list.insertFirst(8);
-//        list.insertFirst(11);
-//
-//
-//        list.insertLast(20);
-//        list.insert(10,3);
-//        list.display();
-////        System.out.println(list.deletefirst());
-////        System.out.println(list.deletelast());
-//        System.out.println(list.delete(2));
-//        System.out.println(list.find(20));
-//        list.display();
+        LL list = new LL();
+        list.insertFirst(3);
+        list.insertFirst(5);
+        list.insertFirst(8);
+        list.insertFirst(11);
+
+
+        list.insertLast(20);
+        list.insert(10,3);
+        list.display();
+//        System.out.println(list.deletefirst());
+//        System.out.println(list.deletelast());
+        System.out.println(list.delete(2));
+        System.out.println(list.find(20));
+        list.display();
 
 //       DLL  list = new DLL();
 //        list.insertfirst(3);
@@ -27,14 +27,18 @@ public class Main {
 //        list.insert(11,20);
 //        list.display();
 
-       CLL list = new CLL();
-       list.insert(20);
-       list.insert(50);
-       list.insert(60);
-       list.insert(40);
-       list.display();
-       list.delete(50);
+//       CLL list = new CLL();
+//       list.insert(20);
+//       list.insert(50);
+//       list.insert(60);
+//       list.insert(40);
+//       list.display();
+//       list.delete(50);
+//        list.display();
+
+        list.insertRec(5,3);
         list.display();
+
 
 
 
