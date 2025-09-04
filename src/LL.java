da745340624924f4e2cd30141e57da1593c513f4
yogarajjuj <yogarajjuju22@gmail.com>
2025-09-04 14:33:58 +0530

    bankani

--- src/LL.java
+++ src/LL.java
@@ -158,20 +158,79 @@ public class LL {
         tail.next=null;
 
     }
+    // merge two sorted list
+
+    public static LL  merge(LL first, LL second){
+       Node f=first.head;
+       Node s= second.head;
+       LL ans = new LL();
+
+       while(f!=null&& s!=null){
+           if(f.value<s.value){
+               ans.insertLast(f.value);
+               f=f.next;
+           } else {
+               ans.insertLast(s.value);
+               s=s.next;
+           }
+       }
+
+       while(f!=null){
+           ans.insertLast(f.value);
+           f=f.next;
+
+       } while (s!=null){
+            ans.insertLast(s.value);
+            s=s.next;
+        }
+       return ans;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     public static void main(String[] args) {
-        LL list = new LL();
+        LL list1 = new LL();
+        LL list2=new LL();
 
-        list.insertLast(1);
-        list.insertLast(1);
-        list.insertLast(1);
-        list.insertLast(2);
-        list.insertLast(3);
-        list.insertLast(3);
-        list.insertLast(3);
-        list.display();
-        list.duplicates();
-        list.display();
+        list1.insertLast(1);
+        list1.insertLast(3);
+        list1.insertLast(5);
+
+        list2.insertLast(1);
+        list2.insertLast(2);
+        list2.insertLast(4);
+        list2.insertLast(9);
+        list2.insertLast(14);
+
+
+//        list.insertLast(1);
+//        list.insertLast(1);
+//        list.insertLast(1);
+//        list.insertLast(2);
+//        list.insertLast(3);
+//        list.insertLast(3);
+//        list.insertLast(3);
+//        list.display();
+//        list.duplicates();
+//        list.display();
+        list1.display();
+        list2.display();
+        LL ans = LL.merge(list1,list2);
+        ans.display();
+
+
 
 
 
