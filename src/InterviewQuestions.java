bcf790212b14f537ec604b60de40ea92575f6283
yogarajjuj <yogarajjuju22@gmail.com>
2025-09-05 12:33:24 +0530

    bankani

--- src/InterviewQuestions.java
+++ src/InterviewQuestions.java
@@ -349,6 +349,24 @@ public class InterviewQuestions {
     }
 
 }
+public ListNode detectCycle(ListNode head) {
+    ListNode fast = head, slow =head;
+    while(fast!=null&& fast.next!=null){
+        slow = slow.next;
+        fast=fast.next.next;
+        if(slow==fast){
+            fast=head;// it is used to start the fast pointer to traverse from the head or we can change the slow = head but we need to move both by 1 only
+
+            while(slow !=fast){
+                fast=fast.next;
+                slow=slow.next;
+            }
+            return fast;// here we can return anyone of the pointers
+        }
+    }
+    return null;
+
+}
 
 
 
